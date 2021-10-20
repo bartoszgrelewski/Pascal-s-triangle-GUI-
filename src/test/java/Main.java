c4e7566ce8d2c4ff7f2c9af74b7a62929d8a0c38
MRkaR-dev <stilus43@gmail.com>
2021-10-20 18:46:22 +0200

    Lab Program 01 test1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int x;
-        int huj;
         while(true){
             System.out.println("Wybierz rodzaj figury ktora chcesz narysowac: ");
             System.out.println("1. Prostokat");
